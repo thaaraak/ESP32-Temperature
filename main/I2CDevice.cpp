5b1395a5c9d5eb413c1e859c1313c32b8ad641da
thaaraak <ghangrene@gmail.com>
2022-01-05 09:18:17 -0600

    dd

--- main/I2CDevice.cpp
+++ main/I2CDevice.cpp
@@ -80,7 +80,9 @@ uint8_t I2CDevice::read_bulk(uint8_t reg, uint8_t* buffer, int len )
 
 	    i2c_master_stop(cmd);
 
-	    ret = i2c_master_cmd_begin(_i2c_num	, cmd, 1000 / portTICK_RATE_MS);
+	    esp_err_t i2c_master_cmd_begin(i2c_port_t i2c_num, i2c_cmd_handle_t cmd_handle, TickType_t ticks_to_wait);
+
+	    ret = i2c_master_cmd_begin( (i2c_port_t)_i2c_num, cmd, (TickType_t) 1000 / portTICK_RATE_MS);
 	    i2c_cmd_link_delete(cmd);
 /*
 		if ( reg == 0 )
